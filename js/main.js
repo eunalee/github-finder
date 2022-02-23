95ffc9ddefd84f7e81615c47a58c1d492b454f53
eunalee <luvlylunalee@gmail.com>
2022-02-23 23:31:35 +0900

    feat: create a event of searchBox like focus, keypress...

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
-console.log("test");
+"use strict";
 
-// searchbox
-// 1. empty
-// display the text (.search__blank)
+const searchBox = document.getElementById("search__box");
 
-// 2. keyword
-// search data with keyword
-// using Github API
-// fetch()
+// when the searchBox is focused, a text disappear
+searchBox.addEventListener("focus", () => {
+  const searchBlankText = document.querySelector(".search__blank");
+  if (searchBlankText.style.display === "block") {
+    searchBlankText.style.display = "none";
+  }
+});
+
+searchBox.addEventListener("keypress", (event) => {
+  // when the enter key is pressed
+  if (event.key === "Enter") {
+    const keyword = document.getElementById("search__box").value;
+    if (keyword === "") {
+      // there is no keyword
+      document.querySelector(".search__blank").style.display = "block";
+    } else {
+      // select user's data with keyword using Github API
+      // fetch()
+    }
+  }
+});
